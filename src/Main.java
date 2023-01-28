96db5d40ab49ba2171d2f1062c02e8b4cdfa005b
kodarov_salavat <kodarov@gmail.com>
2023-01-28 12:58:16 +0400

    HW11\task03

--- src/Main.java
+++ src/Main.java
@@ -44,11 +44,9 @@ public class Main {
         if (deliveryDistance <= 20) System.out.println("Потребуется дней: " + deliveryDay);
         else if (deliveryDistance <= 60) {
             deliveryDay=deliveryDay+1;
-            System.out.println("Потребуется дней: " + deliveryDay);
         }
         else if (deliveryDistance <= 100) {
             deliveryDay=deliveryDay+2;
-            System.out.println("Потребуется дней: " + deliveryDay);
         }
         else System.out.println("Доставки нет");
         return deliveryDay;
@@ -56,6 +54,7 @@ public class Main {
     public static void task3(){
         System.out.println("Задание 3");
         int deliveryDistance = 95;
+        calcDistance(deliveryDistance);
 
     }
 }
