7446a648834301e10fcca510f74d75f2569fa08f
kodarov_salavat <kodarov@gmail.com>
2023-01-28 12:35:19 +0400

    HW11\task01

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,17 @@ public class Main {
         task2();
         task3();
     }
+
+    public static void calcLeapYear(int year) {
+        if ((year % 4)>0 || (year%100) == 0) {
+            System.out.println(year +" год не високосный");
+        }
+        else System.out.println(year +" год високосный");
+    }
     public static void task1(){
         System.out.println("Задание 1");
+        int nowYear = 2022;
+        calcLeapYear(nowYear);
     }
     public static void task2(){
         System.out.println("Задание 2");
